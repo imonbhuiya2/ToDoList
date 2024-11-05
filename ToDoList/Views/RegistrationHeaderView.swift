7ab3de98b867d756900ced5573858064aa4a771a
imonbhuiya2 <imonbhuiya2@gmail.com>
2024-11-05 20:45:18 +0100

    Created registration Page

---
+++ ToDoList/Views/RegistrationHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationHeaderView.swift
+//  ToDoList
+//
+//  Created by Imon Bhuiya on 05.11.24.
+//
+
+import SwiftUI
+
+struct RegistrationHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationHeaderView()
+}
